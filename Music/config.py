60a33c13f352ebfb94b4f5284dfeb8dda56edac3
boni7113 <bonihericahyadi0@gmail.com>
2025-06-07 22:28:31 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "10892147"))
+API_ID = int(getenv('API_ID')) if getenv('API_ID') and getenv('API_ID').isdigit() else 12345678
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '3600'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
